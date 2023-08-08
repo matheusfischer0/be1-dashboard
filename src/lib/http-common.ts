25380c7754f3b1403d552080e0ca591a3c9db7d7
Matheus Fischer <matheus.fischer@beon.studio>
2023-08-08 14:32:51 -0300

    Modified cors to vercel domain

--- src/lib/http-common.ts
+++ src/lib/http-common.ts
@@ -6,9 +6,6 @@ const http = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type'
   },
 }) as AxiosInstance
 
